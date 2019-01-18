eb14c269d1ed98d4893fa503da9fea37d5a95fe2
Sven Fehler <sven.fehler@web.de>
2019-01-18 16:37:17 +0100

    0.2
    - Shut down on boot loop support
    - Option to force keep alive, no matter what happens

--- node-wrap.js
+++ node-wrap.js
@@ -18,7 +18,7 @@ var child, logToConsole = false, crashCounter, bootLoopTimeout = 0, initHR;
  * @prop {String} [logConsoleOutput="none"] Logs all console outputs of the child process to that file, leave null or undefined for no file logging
  * @prop {Boolean} [logTimestamp=true] Whether a timestamp should be added to the above logs
  * @prop {Array<Number>} [restartCodes="no additional codes"] What additional exit codes should invoke a restart
- * @prop {Number} [bootLoopDetection=0] Boot loop prevention mechanism: enter the estimated time in milliseconds it usually takes to INITIALIZE (until an infinite loop of some sort gets started) the child process (0 or leave empty to disable) (higher number = higher stability but also longer delay until the boot loop detection kicks in - if you're unsure or it's unstable, take the biggest number of your measurements)
+ * @prop {Number} [bootLoopDetection=0] Boot loop prevention mechanism: enter the estimated time in milliseconds it usually takes to INITIALIZE (until an infinite loop of some sort gets started) the child process (0 or leave empty to disable) (higher number = higher stability but also longer delay until the boot loop detection kicks in - if you're unsure or it's unstable, take the biggest number of your measurements and/or add a few seconds)
  * @prop {Boolean} [alwaysKeepAlive=false] Set to true to force node-wrap to insistently keep alive / restart the child process as fast and reliably as possible (unaffected by boot loop detection though)
  */
 
